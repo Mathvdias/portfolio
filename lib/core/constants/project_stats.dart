87caee4cb5747c1b49b5ec12f5a4aa74cdf7173a
Matheus Dias <zallpy-matheus.dias@ailos.coop.br>
2026-05-20 23:44:30 -0300

    chore: update coverage and project stats

--- lib/core/constants/project_stats.dart
+++ lib/core/constants/project_stats.dart
@@ -2,6 +2,6 @@
 // Do not edit manually.
 class ProjectStats {
   static const double coverage = 100.00;
-  static const String lastUpdate = '2026-05-20T23:20:25.916693';
-  static const int totalTests = 245;
+  static const String lastUpdate = '2026-05-20T23:33:06.202299';
+  static const int totalTests = 273;
 }
